7da9a7d994ebd4d60457854c2c80a54a7f55901c
danielsnider <danielsnider12@gmail.com>
2018-01-17 16:05:09 -0500

    adding bootstrap and new flask routes

--- server.py
+++ server.py
@@ -1,11 +1,52 @@
+import time
+import os
+from shutil import copyfile
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-from flask import Flask
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 
 @app.route("/")
 def hello():
     f = open("main.html","r") #opens file with name of "test.txt"
     return f.read()
+
+@app.route("/test")
+def test():
+    if int(time.time()) % 2:
+      return "static/images/plots/A.jpg"
+    else:
+      return "static/images/plots/B.jpg"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        # if file and allowed_file(file.filename):
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(request.url)
+
 
 if __name__ == "__main__":
     app.run()
