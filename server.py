aed07853c27e66786f6b02409a0e693e646f2ed0
danielsnider <danielsnider12@gmail.com>
2018-01-19 17:43:43 -0500

    Cleanup and bugfix for multiple file upload

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import time
-import glob  
+import glob
 import os
 from IPython import embed
 from shutil import copyfile
@@ -7,29 +7,20 @@ from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route("/")
 def hello():
-    images=list(glob.glob('static/*g'))
-    print images
-    return render_template('main.html', images=images, name='bessa!')
-
-@app.route("/test")
-def test():
-    if int(time.time()) % 2:
-      return "static/images/plots/A.jpg"
-    else:
-      return "static/images/plots/B.jpg"
+    images=list(glob.glob('static/uploads/*g'))
+    return render_template('main.html', images=images)
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+      filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -38,18 +29,17 @@ def upload_file():
         if 'file' not in request.files:
             print('No file part')
             return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            print('No selected file')
-            return redirect(request.url)
-        # if file and allowed_file(file.filename):
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(request.url)
-
+        files = request.files.getlist("file")
+        for file in files:
+          # if user does not select file, browser also
+          # submit a empty part without filename
+          if file.filename == '':
+              print('No selected file')
+              return redirect(request.url)
+          if file and allowed_file(file.filename):
+              filename = secure_filename(file.filename)
+              file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(request.url)
 
 if __name__ == "__main__":
     app.run()
