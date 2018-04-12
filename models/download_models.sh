41d09b07ff569ce3cff0ffe2f01efc54f0b2a64f
danielsnider <danielsnider12@gmail.com>
2018-04-12 16:42:48 -0400

    header links

--- models/download_models.sh
+++ models/download_models.sh
@@ -1,2 +1,8 @@
+
 wget http://download.tensorflow.org/models/resnet_v2_101_2017_04_14.tar.gz
+tar -xvf resnet_v2_101_2017_04_14.tar.gz
+rm resnet_v2_101_2017_04_14.tar.gz
+
 wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+tar -xvf vgg_16_2016_08_28.tar.gz
+rm vgg_16_2016_08_28.tar.gz
