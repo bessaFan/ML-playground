9bfaed4b674e79e79deb31318585c71fcccf52f2
danielsnider <danielsnider12@gmail.com>
2018-04-26 23:40:23 -0400

    download resnet 152

--- models/download_models.sh
+++ models/download_models.sh
@@ -1,3 +1,6 @@
+wget http://download.tensorflow.org/models/resnet_v2_152_2017_04_14.tar.gz
+tar -xvf resnet_v2_152_2017_04_14.tar.gz
+rm resnet_v2_152_2017_04_14.tar.gz
 
 wget http://download.tensorflow.org/models/resnet_v2_101_2017_04_14.tar.gz
 tar -xvf resnet_v2_101_2017_04_14.tar.gz
