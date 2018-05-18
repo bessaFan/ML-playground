5a0f841b7b5598080f1eabfb3b4981dde471f469
bessafan <bessafan01@gmail.com>
2018-05-18 15:33:35 -0400

    updated downlod_models.sh to include all the models that would be used

--- models/download_models.sh
+++ models/download_models.sh
@@ -13,3 +13,20 @@ rm vgg_16_2016_08_28.tar.gz
 wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
 tar -xvf inception_v3_2016_08_28.tar.gz
 rm inception_v3_2016_08_28.tar.gz
+
+
+wget http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz
+tar -xvf inception_v4_2016_09_09.tar.gz
+rm inception_v4_2016_09_09.tar.gz
+
+wget http://download.tensorflow.org/models/resnet_v2_152_2017_04_14.tar.gz
+tar -xvf resnet_v2_152_2017_04_14.tar.gz
+rm resnet_v2_152_2017_04_14.tar.gz
+
+wget https://storage.googleapis.com/download.tensorflow.org/models/nasnet-a_large_04_10_2017.tar.gz
+tar -xvf nasnet-a_large_04_10_2017.tar.gz
+rm nasnet-a_large_04_10_2017.tar.gz
+
+wget https://storage.googleapis.com/download.tensorflow.org/models/pnasnet-5_large_2017_12_13.tar.gz
+tar -xvf pnasnet-5_large_2017_12_13.tar.gz
+rm pnasnet-5_large_2017_12_13.tar.gz
