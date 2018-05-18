ac622b36c5dbdccdfef17755cd8d9be63af54469
bessafan <bessafan01@gmail.com>
2018-05-18 15:29:13 -0400

    updated downlod_models.sh to include inceptionv3

--- models/download_models.sh
+++ models/download_models.sh
@@ -9,3 +9,7 @@ rm resnet_v2_101_2017_04_14.tar.gz
 wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
 tar -xvf vgg_16_2016_08_28.tar.gz
 rm vgg_16_2016_08_28.tar.gz
+
+wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+tar -xvf inception_v3_2016_08_28.tar.gz
+rm inception_v3_2016_08_28.tar.gz
